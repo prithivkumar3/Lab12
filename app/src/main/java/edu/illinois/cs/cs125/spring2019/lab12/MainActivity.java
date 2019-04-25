2ae2cf783898ceb621e7d331067243069fc269be
prithivkumar3 <pskumar2@illinois.edu>
2019-04-25 05:06:16 -0500

    login activity_main

--- app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
@@ -9,17 +9,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
-/*import android.util.Log;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;*/
 
 /**
  * Main activity for Final Project.
@@ -37,9 +26,25 @@ public final class MainActivity extends AppCompatActivity {
     private static Attack lastAttack;
 
     /**
+     * last attack used on computer.
+     */
+    private static Attack lastAttackA;
+
+    /**
+     * last attack used on player.
+     */
+    private static Attack lastAttackB;
+
+    /**
      * number of types of benders.
      */
     private static final int NUM_TYPES = 4;
+
+    /**
+     * wait time.
+     */
+    private static final int TIME = 1000;
+
     /**
      * types of benders for computer to randomly choose.
      */
@@ -54,27 +59,124 @@ public final class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         findViewById(R.id.check).setVisibility(View.GONE);
-
         final TextInputEditText inputName = findViewById(R.id.inputName);
-
         final TextInputEditText inputType = findViewById(R.id.inputType);
-
         final Button startGame = findViewById(R.id.start);
-
-        startGame.setOnClickListener(v -> {
+        startGame.setOnClickListener(b -> {
             try {
                 String type = inputType.getText().toString();
                 switch (type) {
-                    case "fire" : break;
-                    case "water" : break;
-                    case "earth" : break;
-                    case "air" : break;
-                    default : throw new NullPointerException();
+                    case "fire":
+                        break;
+                    case "water":
+                        break;
+                    case "earth":
+                        break;
+                    case "air":
+                        break;
+                    default:
+                        throw new NullPointerException();
                 }
                 Bender player = new Bender(type, inputName.getText().toString());
                 Random rand = new Random();
                 Bender opponent = new Bender(types[rand.nextInt(NUM_TYPES)], "Computer");
-                runGame(player, opponent);
+                setContentView(R.layout.activity_main);
+                TextView playerName = findViewById(R.id.player);
+                playerName.setText(player.getName());
+                TextView opponentName = findViewById(R.id.opponent);
+                opponentName.setText(opponent.getName());
+                ImageView playerI = findViewById(R.id.playerImage);
+                switch (player.getType()) {
+                    case "fire":
+                        playerI.setImageResource(R.drawable.fire);
+                        break;
+                    case "water":
+                        playerI.setImageResource(R.drawable.water);
+                        break;
+                    case "earth":
+                        playerI.setImageResource(R.drawable.earth);
+                        break;
+                    case "air":
+                        playerI.setImageResource(R.drawable.air);
+                        break;
+                    default:
+                        break;
+                }
+                ImageView opponentI = findViewById(R.id.opponentImage);
+                switch (opponent.getType()) {
+                    case "fire":
+                        opponentI.setImageResource(R.drawable.fire);
+                        break;
+                    case "water":
+                        opponentI.setImageResource(R.drawable.water);
+                        break;
+                    case "earth":
+                        opponentI.setImageResource(R.drawable.earth);
+                        break;
+                    case "air":
+                        opponentI.setImageResource(R.drawable.air);
+                        break;
+                    default:
+                        break;
+                }
+                Button one = findViewById(R.id.attackOne);
+                Button two = findViewById(R.id.attackTwo);
+                Button three = findViewById(R.id.attackThree);
+                Button four = findViewById(R.id.attackFour);
+                one.setText(player.getAttacks()[0].getName());
+                two.setText(player.getAttacks()[NUM_TYPES - 2 - 1].getName());
+                three.setText(player.getAttacks()[NUM_TYPES - 2].getName());
+                four.setText(player.getAttacks()[NUM_TYPES - 1].getName());
+                TextView health = findViewById(R.id.health);
+                String pH = "Health: " + player.getHealth();
+                health.setText(pH);
+                TextView oppHealth = findViewById(R.id.oppHealth);
+                String oH = "Health: " + opponent.getHealth();
+                oppHealth.setText(oH);
+                one.setOnClickListener(v -> {
+                    executeAttack(player.getAttacks()[0], opponent);
+                    finish(player, opponent);
+                    try {
+                        Thread.sleep(TIME);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    opponentExecuteAttack(opponent.getAttacks()[rand.nextInt(NUM_TYPES)], player);
+                    finish(player, opponent);
+                });
+                two.setOnClickListener(v -> {
+                    executeAttack(player.getAttacks()[1], opponent);
+                    finish(player, opponent);
+                    try {
+                        Thread.sleep(TIME);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    opponentExecuteAttack(opponent.getAttacks()[rand.nextInt(NUM_TYPES)], player);
+                    finish(player, opponent);
+                });
+                three.setOnClickListener(v -> {
+                    executeAttack(player.getAttacks()[2], opponent);
+                    finish(player, opponent);
+                    try {
+                        Thread.sleep(TIME);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    opponentExecuteAttack(opponent.getAttacks()[rand.nextInt(NUM_TYPES)], player);
+                    finish(player, opponent);
+                });
+                four.setOnClickListener(v -> {
+                    executeAttack(player.getAttacks()[NUM_TYPES - 1], opponent);
+                    finish(player, opponent);
+                    try {
+                        Thread.sleep(TIME);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    opponentExecuteAttack(opponent.getAttacks()[rand.nextInt(NUM_TYPES)], player);
+                    finish(player, opponent);
+                });
             } catch (NullPointerException e) {
                 findViewById(R.id.check).setVisibility(View.VISIBLE);
             }
@@ -82,137 +184,118 @@ public final class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Starts game between player and computer.
+     * finish off the program.
      * @param player player
-     * @param opponent computer playing against player
+     * @param opponent opponent
      */
-    public void runGame(final Bender player, final Bender opponent) {
-        setContentView(R.layout.activity_main);
-        final Button restart = findViewById(R.id.start);
-        restart.setOnClickListener(v -> {
-            //setContentView(R.layout.login);
-            //findViewById(R.id.check).setVisibility(View.GONE);
-            //finish();
-        });
-        TextView playerName = findViewById(R.id.player);
-        playerName.setText(player.getName());
-        TextView opponentName = findViewById(R.id.opponent);
-        opponentName.setText(opponent.getName());
-        ImageView playerI = findViewById(R.id.playerImage);
-        switch (player.getType()) {
-            case "fire" : playerI.setImageResource(R.drawable.fire);
-                break;
-            case "water" : playerI.setImageResource(R.drawable.water);
-                break;
-            case "earth" : playerI.setImageResource(R.drawable.earth);
-                break;
-            case "air" : playerI.setImageResource(R.drawable.air);
-                break;
-            default : break;
-        }
-        ImageView opponentI = findViewById(R.id.opponentImage);
-        switch (opponent.getType()) {
-            case "fire" : opponentI.setImageResource(R.drawable.fire);
-                break;
-            case "water" : opponentI.setImageResource(R.drawable.water);
-                break;
-            case "earth" : opponentI.setImageResource(R.drawable.earth);
-                break;
-            case "air" : opponentI.setImageResource(R.drawable.air);
-                break;
-            default : break;
-        }
-        Button one = findViewById(R.id.attackOne);
-        Button two = findViewById(R.id.attackTwo);
-        Button three = findViewById(R.id.attackThree);
-        Button four = findViewById(R.id.attackFour);
-        one.setText(player.getAttacks()[0].getName());
-        two.setText(player.getAttacks()[NUM_TYPES - 2 - 1].getName());
-        three.setText(player.getAttacks()[NUM_TYPES - 2].getName());
-        four.setText(player.getAttacks()[NUM_TYPES - 1].getName());
-        TextView health = findViewById(R.id.health);
-        String pH = "Health: " + player.getHealth();
-        health.setText(pH);
-        TextView oppHealth = findViewById(R.id.oppHealth);
-        String oH = "Health: " + opponent.getHealth();
-        oppHealth.setText(oH);
-        TextView display = findViewById(R.id.log);
-        while (player.getHealth() > 0 && opponent.getHealth() > 0) {
-            one.setOnClickListener(v -> {
-                executeAttack(player.getAttacks()[0], player, opponent);
-            });
-            two.setOnClickListener(v -> {
-                executeAttack(player.getAttacks()[1], player, opponent);
-            });
-            three.setOnClickListener(v -> {
-                executeAttack(player.getAttacks()[2], player, opponent);
-            });
-            four.setOnClickListener(v -> {
-                executeAttack(player.getAttacks()[NUM_TYPES - 1], player, opponent);
-            });
-            pH = "Health: " + player.getHealth();
-            health.setText(pH);
-            oH = "Health: " + opponent.getHealth();
-            oppHealth.setText(oH);
-        }
-        String won = "YOU WIN!";
-        String lost = "YOU LOSE";
+    public void finish(final Bender player, final Bender opponent) {
         if (player.getHealth() <= 0) {
-            display.setText(won);
+            TextView display = findViewById(R.id.log);
+            String state = "YOU LOSE";
+            display.setText(state);
         } else if (opponent.getHealth() <= 0) {
-            display.setText(lost);
+            TextView display = findViewById(R.id.log);
+            String state = "YOU WIN!";
+            display.setText(state);
+        } else {
+            return;
         }
+        configureTitleButton();
+    }
+    /**
+     * return to title screen.
+     */
+    private void configureTitleButton() {
+        Button restart = findViewById(R.id.restart);
+        restart.setOnClickListener(v -> finish());
     }
 
     /**
      * execute a chosen attack.
      * @param current attack chosen by player
-     * @param player player using attack
      * @param opponent player receiving attack
      */
-    public void executeAttack(final Attack current, final Bender player, final Bender opponent) {
+    public void executeAttack(final Attack current, final Bender opponent) {
         String no = "You can't choose the same attack twice in a row.";
         TextView display = findViewById(R.id.log);
-        if (current.equals(lastAttack)) {
+        if (current.equals(lastAttackA)) {
             display.setText(no);
-            return;
-        }
-        String oof = "The last attack was a defensive move! This attack did no damage!";
-        if (lastAttack.getType().equals("def")) {
-            display.setText(oof);
-            lastAttack = current;
-            return;
-        }
-        String superEffective = "The attack was super effective!";
-        if (player.getType().equals("earth") && opponent.getType().equals("air")) {
-            display.setText(superEffective);
-            opponent.setHealth(opponent.getHealth() - (NUM_TYPES - 1) * current.getDamage());
-            lastAttack = current;
-            return;
-        }
-        if (player.getType().equals("water") && opponent.getType().equals("fire")) {
-            display.setText(superEffective);
-            opponent.setHealth(opponent.getHealth() - (NUM_TYPES - 1) * current.getDamage());
-            lastAttack = current;
-            return;
-        }
-        if (player.getType().equals("fire") && opponent.getType().equals("water")) {
-            display.setText(superEffective);
-            opponent.setHealth(opponent.getHealth() - (NUM_TYPES - 1) * current.getDamage());
-            lastAttack = current;
-            return;
-        }
-        if (player.getType().equals("air") && opponent.getType().equals("earth")) {
-            display.setText(superEffective);
-            opponent.setHealth(opponent.getHealth() - (NUM_TYPES - 1) * current.getDamage());
-            lastAttack = current;
             return;
         }
         String whatIsHappening = "You used " + current.getName() + "!";
         display.setText(whatIsHappening);
+        try {
+            Thread.sleep(TIME);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        String oof = "The last attack was a defensive move! This attack did no damage!";
+        if (lastAttack != null && lastAttack.getType().equals("def")) {
+            display.setText(oof);
+            lastAttackA = current;
+            lastAttack = current;
+            return;
+        }
+        String superEffective = "Your " + current.getName() + " was super effective!";
+        if (current.getOpp() != null && current.getOpp().equals(opponent.getType())) {
+            display.setText(superEffective);
+            opponent.setHealth(opponent.getHealth() - (NUM_TYPES - 1) * current.getDamage());
+            TextView oppHealth = findViewById(R.id.oppHealth);
+            String oH = "Health: " + opponent.getHealth();
+            oppHealth.setText(oH);
+            lastAttackA = current;
+            lastAttack = current;
+            return;
+        }
         opponent.setHealth(opponent.getHealth() - current.getDamage());
+        TextView oppHealth = findViewById(R.id.oppHealth);
+        String oH = "Health: " + opponent.getHealth();
+        oppHealth.setText(oH);
+        lastAttackA = current;
         lastAttack = current;
+    }
 
+    /**
+     * execute a chosen attack.
+     * @param current attack chosen by player
+     * @param opponent player receiving attack
+     */
+    public void opponentExecuteAttack(final Attack current, final Bender opponent) {
+        if (current.equals(lastAttackB)) {
+            return;
+        }
+        TextView display = findViewById(R.id.log);
+        String whatIsHappening = "The opponent used " + current.getName() + "!";
+        display.setText(whatIsHappening);
+        try {
+            Thread.sleep(TIME);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        String oof = "The last attack was a defensive move! This attack did no damage!";
+        if (lastAttack != null && lastAttack.getType().equals("def")) {
+            display.setText(oof);
+            lastAttackB = current;
+            lastAttack = current;
+            return;
+        }
+        String superEffective = "The opponent's " + current.getName() + " was super effective!";
+        if (current.getOpp() != null && current.getOpp().equals(opponent.getType())) {
+            display.setText(superEffective);
+            opponent.setHealth(opponent.getHealth() - (NUM_TYPES - 1) * current.getDamage());
+            TextView health = findViewById(R.id.health);
+            String pH = "Health: " + opponent.getHealth();
+            health.setText(pH);
+            lastAttackB = current;
+            lastAttack = current;
+            return;
+        }
+        opponent.setHealth(opponent.getHealth() - current.getDamage());
+        TextView health = findViewById(R.id.health);
+        String pH = "Health: " + opponent.getHealth();
+        health.setText(pH);
+        lastAttackB = current;
+        lastAttack = current;
     }
 
     /*/**
