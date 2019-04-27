343ce1322de00f1874021540e80101203fd46c5e
prithivkumar3 <pskumar2@illinois.edu>
2019-04-27 15:58:08 -0500

    nice

--- app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
@@ -3,10 +3,19 @@ package edu.illinois.cs.cs125.spring2019.lab12;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Random;
 
@@ -338,13 +347,13 @@ public final class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
     }
-
+*/
     /**
      * Make a call to the IP geolocation API.
      *
      * @param ipAddress IP address to look up
      */
-    /*void startAPICall(final String ipAddress) {
+    public void startAPICall(final String ipAddress) {
         try {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
@@ -373,11 +382,12 @@ public final class MainActivity extends AppCompatActivity {
      *
      * @param response response from our IP geolocation API.
      *
+     */
     void apiCallDone(final JSONObject response) {
         try {
             Log.d(TAG, response.toString(2));
             // Example of how to pull a field off the returned JSON object
             Log.i(TAG, response.get("hostname").toString());
         } catch (JSONException ignored) { }
-    }*/
+    }
 }
